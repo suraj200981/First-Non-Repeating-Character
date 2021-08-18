fab83e6aaf20ec4cfa7a1ff92dc139588802e1c0
Suraj Sharma <suraj.sharma1998@yahoo.com>
2021-08-18 20:19:28 +0100

    bit of progress

--- firstNonRepeating.js
+++ firstNonRepeating.js
@@ -1,1 +1,21 @@
-console.log("test");
+function main (input){
+
+//initalising input as string    
+var str = "";
+str = input;
+
+
+var alphabetHashMap = new Map ([
+    
+])
+for(var i=0; i<str.length; i++){
+
+    
+
+}
+
+
+return str;
+}
+
+console.log(main("abcbbac"));
