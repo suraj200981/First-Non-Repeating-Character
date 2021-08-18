0b67848b397ce5a07a552de35b1527d54ed85ea7
Suraj Sharma <suraj.sharma1998@yahoo.com>
2021-08-18 21:14:00 +0100

    different approach

--- firstNonRepeating.js
+++ firstNonRepeating.js
@@ -1,21 +1,23 @@
-function main (input){
+function main(input) {
+  //initalising input as string
+  var str = "";
+  str = input;
 
-//initalising input as string    
-var str = "";
-str = input;
+  var nonRepeatingChar = "";
 
+  for (var i = 0; i < str.length; i++) {
+    for (var x = i + 1; x < str.length - 1; x++) {
+      console.log(str[i] + "" + str[x]);
+      if (str[i] === str[x]) {
+        nonRepeatingChar = str[x];
+      }
+    }
+  }
+  if (nonRepeatingChar === "") {
+    nonRepeatingChar = "-1";
+  }
 
-var alphabetHashMap = new Map ([
-    
-])
-for(var i=0; i<str.length; i++){
-
-    
-
-}
-
-
-return str;
+  return nonRepeatingChar;
 }
 
 console.log(main("abcbbac"));
